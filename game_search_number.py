e54fe432338e853bfd08d60c7e82ecbfd6b52a61
Valerii57586 <120032748+Valerii57586@users.noreply.github.com>
2023-04-09 13:29:53 +0300

    Add files via upload

---
+++ game_search_number.py
@@ -0,0 +1,22 @@
+import random 
+def search_number():
+
+
+
+    while True :
+        player1 = int(random.randint(1,5))
+        player2 = int(input('угадай число от 1 до 5 '))
+        otvet = player1 - player2
+        if player1 == player2 :
+            print('угадал удача на твоей стороне')
+            yes_or_no = input('хотите ли вы продолжить ')
+        else:
+            print(f'не угадал тебе не хватило {otvet} попробуй еще раз мало ли в следующий раз провезет')
+            yes_or_no = input('хотите ли вы продолжить ')
+        if yes_or_no == 'нет':
+            break
+if __name__ == '__main__' :
+    search_number()
+
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+import game_search_number
+print('наш католог:','угадай число','стань ближе к нужному','дота пикер')
+game = input('выбери игру которую хочешь погеймить')
+if game == 'угадай число':
+    game_search_number.search_number()
+
+
+
+
+
+
